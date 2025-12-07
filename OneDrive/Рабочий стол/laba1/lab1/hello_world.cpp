371abb1f16b7b3e57f1a74fc60bc58091a14edc9
Oleg- <oleg-16042002@mail.ru>
2025-12-07 19:01:03 +0400

    Исправление: удаление using namespace std и добавление stg::

--- OneDrive/Рабочий стол/laba1/lab1/hello_world.cpp
+++ OneDrive/Рабочий стол/laba1/lab1/hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-using namespace std;  // Плохой стиль
+#include <string>
 
 int main() {
-    string name;
-    cout << "Введите ваше имя: ";
-    cin >> name;
-    cout << "Hello world from " << name << endl;
+    std::string name;
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
