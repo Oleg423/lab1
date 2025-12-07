b351337e9d159522cdbfe58315f95dcbc77a7e29
Oleg- <oleg-16042002@mail.ru>
2025-12-07 19:04:27 +0400

    Добавление комметариев в код

--- OneDrive/Рабочий стол/laba1/lab1/hello_world.cpp
+++ OneDrive/Рабочий стол/laba1/lab1/hello_world.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <string>
+#include <string>  // Для использования std::string
+
+/*
+ * Программа Hello World с вводом имени пользователя
+ * Лабораторная работа №2
+ * Автор: [Ваше Имя]
+ */
 
 int main() {
-    std::string name;
+    std::string name;  // Переменная для хранения имени
+    
+    // Запрос имени пользователя
     std::cout << "Введите ваше имя: ";
     std::cin >> name;
+    
+    // Вывод приветственного сообщения
     std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    
+    return 0;  // Успешное завершение программы
 }
